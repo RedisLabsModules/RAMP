027f73cb86946138b0fbec963511b37428e6f6c6
Dvir Volk <dvirsky@gmail.com>
2017-05-10 18:05:26 +0300

    write only module name as output by default

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ Which contains:
 
 setup(
     name='ramp-packer',
-    version='1.2.2',
+    version='1.2.3',
     description='Packs for Redis modules into a distributable format',
     author='RedisLabs',
     url='https://github.com/redislabs/RAMP',
