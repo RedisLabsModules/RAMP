8c06c3b231920caf9788e37074b4dabed501a9da
swilly22 <roi@redislabs.com>
2017-05-10 11:39:37 +0300

    embed long desc within setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,50 @@
 from setuptools import setup, find_packages
 
+LONG_DESC = """
+# RAMP
+Redis Automatic Module Packaging
+
+Similar to `npm init`, this packer bundles Redis Modules for later distribution.
+
+It gathers information from modules e.g.
+module's name, command list, version and additional metadata.
+
+# Prerequisites
+Make sure redis-server is on your PATH
+
+```sh
+export PATH=$PATH:<PATH_TO_REDIS>
+```
+
+# Usage
+## Interactive mode
+
+```sh
+python module_packer <PATH_TO_RedisModule.so>
+```
+
+## Command line mode
+
+```sh
+python module_packer <PATH_TO_RedisModule.so> -a <author> -e <email> -ar <architecture> -d <description> -ho <homepage> -l <license> -ex <extras> -c <cmdargs> -r <redis-min-version>
+```
+
+For Help
+
+```sh
+python module_packer <PATH_TO_RedisModule.so> -h
+```
+
+## Output
+module_packer generates module.zip
+
+Which contains:
+
+    1. RedisModule.so - original module
+    2. Module.json - module's metadata
+
+"""
+
 setup(
     name='ramp-packer',
     version='1.0',
@@ -8,7 +53,7 @@ setup(
     url='https://github.com/redislabs/RAMP',
     scripts=['packer/module_packer'],
     license='BSD 2-clause',
-    long_description=open('README.md').read(),
+    long_description=LONG_DESC,
     packages=find_packages(),
     install_requires=['redis']
 )
