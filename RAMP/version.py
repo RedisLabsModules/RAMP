739e367bba7714a3e8ac5172d1e897e07e5ae548
swilly22 <roi@redislabs.com>
2017-11-27 09:03:37 +0200

    simplified version

--- RAMP/version.py
+++ RAMP/version.py
@@ -1,6 +1,4 @@
-import semantic_version
-
 MAJOR_VERSION = 1
 MINOR_VERSION = 3
-PATCH_VERSION = 8
-VERSION = semantic_version.Version('{}.{}.{}'.format(MAJOR_VERSION, MINOR_VERSION, PATCH_VERSION))
+PATCH_VERSION = 9
+VERSION = '{}.{}.{}'.format(MAJOR_VERSION, MINOR_VERSION, PATCH_VERSION)
