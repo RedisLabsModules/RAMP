751011674a5333d910463dba4caa28103dd055cb
rafie <rafi@redislabs.com>
2019-08-13 17:12:39 +0300

    Refacoring for arguments overriding manifest

--- RAMP/packer.py
+++ RAMP/packer.py
@@ -3,10 +3,11 @@ import json
 import zipfile
 import yaml
 import semantic_version
+import tempfile
+from subprocess import Popen, PIPE
 
 import RAMP.module_metadata as module_metadata
 from RAMP.commands_discovery import discover_modules_commands
-from subprocess import Popen, PIPE
 
 def version_to_semantic_version(version):
     """
@@ -30,7 +31,7 @@ def set_defaults(module_path):
     metadata = module_metadata.create_default_metadata(module_path)
     return metadata
 
-def manifest_mode(metadata, manifest):
+def init_from_manifest(metadata, manifest):
     """
     Creates module metadata from user provided menifest file
     """
@@ -49,45 +50,35 @@ def archive(module_path, metadata, archive_name='module.zip'):
     """
     Archives both module and module metadata.
     """
-    archive_name = archive_name.format(**metadata)
-    with open('module.json', 'w') as outfile:
+    with tempfile.NamedTemporaryFile(mode='w', prefix='ramp.json', delete=False) as outfile:
+        jfile = outfile.name
+        archive_name = archive_name.format(**metadata)
         json.dump(metadata, outfile, indent=4, sort_keys=True)
 
-    archive_file = zipfile.ZipFile(archive_name, 'w', zipfile.ZIP_DEFLATED)
     try:
-        archive_file.write(module_path, metadata["module_file"])
-        archive_file.write('module.json')
-        print(archive_name)
+        with zipfile.ZipFile(archive_name, 'w', zipfile.ZIP_DEFLATED) as archive_file:
+            archive_file.write(module_path, metadata["module_file"])
+            archive_file.write(jfile, arcname='module.json')
+            print(archive_name)
     finally:
-        archive_file.close()
-        os.remove("module.json")
+        os.remove(jfile)
 
-def package(module, output, verbose, manifest, display_name, module_name, author,
-            email, architecture, description, homepage, license, cmdargs,
-            redis_min_version, redis_pack_min_version, config_command, os, os_list, capabilities,
-            print_filename_only, exclude_commands, overide_command):
+
+def package(module, **args):
     module_path = module
     metadata = set_defaults(module_path)
 
+    manifest = args['manifest']
     if manifest:
-        manifest_mode(metadata, manifest)
+        init_from_manifest(metadata, manifest)
 
-    if architecture: metadata["architecture"] = architecture
-    if os: metadata["os"] = os
-    if os_list: metadata["os_list"] = os_list
-    if display_name: metadata["display_name"] = display_name
-    if author: metadata["author"] = author
-    if email: metadata["email"] = email
-    if description: metadata["description"] = description
-    if homepage: metadata["homepage"] = homepage
-    if license: metadata["license"] = license
-    if cmdargs: metadata["command_line_args"] = cmdargs
-    if redis_min_version: metadata["min_redis_version"] = redis_min_version
-    if redis_pack_min_version: metadata["min_redis_pack_version"] = redis_pack_min_version
-    if capabilities: metadata["capabilities"] = capabilities
-    if config_command: metadata["config_command"] = config_command
-    if exclude_commands: metadata["exclude_commands"] = exclude_commands
-    if overide_command: metadata["overide_command"] = overide_command
+    nonkeys = dict.fromkeys(['manifest', 'verbose', 'print_filename_only', 'output', 'override_command'], 1)
+    for key, value in args.iteritems():
+        if key in nonkeys or key not in module_metadata.FIELDS:
+            continue
+        if value is None or value == []:
+            continue
+        metadata[key] = value
 
     # Load module into redis and discover its commands
     module = discover_modules_commands(module_path, metadata["command_line_args"])
@@ -109,26 +100,28 @@ def package(module, output, verbose, manifest, display_name, module_name, author
             print('overiding %s with %s' % (str(metadata["commands"][overide_index[0]]), str(overide)))
         metadata["commands"][overide_index[0]] = overide
 
+    module_name = args['module_name']
     if module_name:
         metadata["module_name"] = module_name
-        
+
     try:
         p = Popen('git rev-parse HEAD'.split(' '), stdin=PIPE, stdout=PIPE, stderr=PIPE)
         git_sha, err = p.communicate()
-        if err != '':
-            print("could not extract git sha %s" % err)
+        if p.returncode != 0:
+            print("could not extract git sha {}".format(err))
         else:
             metadata["git_sha"] = git_sha.strip()
     except Exception:
-        print("could not extract git sha %s" % err)
+        print("could not extract git sha {}".format(err))
 
-    if print_filename_only:
+    output = args['output']
+    if args['print_filename_only']:
         # For scripts, it might be helpful to know the formatted filename
         # ahead of time, so that it can manipulate it later on.
         print(output.format(**metadata))
         return 0
 
-    if verbose:
+    if args['verbose']:
         print("Module Metadata:")
         print(json.dumps(metadata, indent=2))
 
--- RAMP/ramp.py
+++ RAMP/ramp.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 
 import os
+import sys
 import json
 import click
+
+# sys.path.insert(0, os.path.join(os.path.dirname(__file__), "../../readies"))
+# import paella
 
 from RAMP.packer import package
 from RAMP.unpacker import validate_bundle, unpack as unpack_bundle
@@ -29,6 +33,7 @@ def jsons_str_tuple_to_jsons_tuple(ctx, param, value):
         return [json.loads(a) for  a in value]
 
 @click.group()
+@click.version_option(version=VERSION)
 def ramp():
     pass
 
@@ -52,7 +57,7 @@ def validate(bundle):
 @click.argument('bundle')
 def unpack(bundle):
     metadata, module = unpack_bundle(bundle)
-    module_metadata_file_name = os.path.join(os.getcwd(), metadata['module_name']+'.json')
+    module_metadata_file_name = os.path.join(os.getcwd(), metadata['module_name'] + '.json')
     module_file_name = os.path.join(os.getcwd(), metadata['module_file'])
 
     with open(module_metadata_file_name, 'w') as outfile:
@@ -68,35 +73,28 @@ def unpack(bundle):
 
 @ramp.command()
 @click.argument('module')
-@click.option('--output', '-o', default='module.zip', help='output file name')
-@click.option('--verbose', '-v', is_flag=True, help='verbose mode: print the resulting metadata')
+@click.option('--output', '-o', help='output file name')
+@click.option('--verbose', '-v', is_flag=True, default=None, help='verbose mode: print the resulting metadata')
 @click.option('--manifest', '-m', type=click.File('rb'), help='generate package from manifest')
-@click.option('--display-name', '-d', 'display_name', default=module_metadata.DISPLAY_NAME, help='name for display purposes')
-@click.option('--module-name', '-n', 'module_name', help='module name')
-@click.option('--author', '-a', default=module_metadata.AUTHOR, help='module author')
-@click.option('--email', '-e', default=module_metadata.EMAIL, help='author\'s email')
-@click.option('--architecture', '-A', default=module_metadata.ARCHITECTURE, help='module compiled on i386/x86_64 arch')
-@click.option('--description', '-D', default=module_metadata.DESCRIPTION, help='short description')
-@click.option('--homepage', '-h', default=module_metadata.HOMEPAGE, help='module homepage')
-@click.option('--license', '-l', default=module_metadata.LICENSE, help='license')
-@click.option('--cmdargs', '-c', default=module_metadata.COMMAND_LINE_ARGS, help='module command line arguments')
-@click.option('--redis-min-version', '-r', 'redis_min_version', default=module_metadata.MIN_REDIS_VERSION, help='redis minimum version')
-@click.option('--redis-pack-min-version', '-R', 'redis_pack_min_version', default=module_metadata.MIN_REDIS_PACK_VERSION, help='redis pack minimum version')
-@click.option('--config-command', '-cc', default=module_metadata.CONFIG_COMMAND, help='command used to configure module args at runtime')
-@click.option('--os', '-O', default=module_metadata.OS, help='build target OS (Darwin/Linux)')
-@click.option('--os-list', callback=comma_seperated_to_list, help='build target OS (Darwin/Linux)')
+@click.option('--display-name', '-d', 'display_name', default=None, help='name for display purposes')
+@click.option('--module-name', '-n', 'module_name', default=None, help='module name')
+@click.option('--author', '-a', default=None, help='module author')
+@click.option('--email', '-e', default=None, help='author\'s email')
+@click.option('--architecture', '-A', default=None, help='module compiled on i386/x86_64 arch')
+@click.option('--description', '-D', default=None, help='short description')
+@click.option('--homepage', '-h', default=None, help='module homepage')
+@click.option('--license', '-l', default=None, help='license')
+@click.option('--cmdargs', '-c', 'command_line_args', default=None, help='module command line arguments')
+@click.option('--redis-min-version', '-r', default=None, help='redis minimum version')
+@click.option('--redis-pack-min-version', '-R', default=None, help='redis pack minimum version')
+@click.option('--config-command', '-cc', default=None, help='command used to configure module args at runtime')
+@click.option('--os', '-O', default=None, help='build target OS (Darwin/Linux)')
 @click.option('--capabilities', '-C', callback=comma_seperated_to_list, help='comma seperated list of module capabilities')
-@click.option('--print-filename-only', '-P', is_flag=True, help="Print package path, but don't generate file")
+@click.option('--print-filename-only', '-P', is_flag=True, default=None, help="Print package path, but don't generate file")
 @click.option('--exclude-commands', '-E', callback=comma_seperated_to_list, help='comma seperated list of exclude commands')
 @click.option('--overide-command', multiple=True, callback=jsons_str_tuple_to_jsons_tuple, help='gets a command json representation and overide it on the module json file')
-def pack(module, output, verbose, manifest, display_name, module_name, author,
-         email, architecture, description, homepage, license, cmdargs,
-         redis_min_version, redis_pack_min_version, config_command, os, os_list, capabilities,
-         print_filename_only, exclude_commands, overide_command):
-    return package(module, output, verbose, manifest, display_name, module_name, author,
-                   email, architecture, description, homepage, license, cmdargs,
-                   redis_min_version, redis_pack_min_version, config_command, os, os_list, capabilities,
-                   print_filename_only, exclude_commands, overide_command)
+def pack(module, *args, **kwargs):
+    return package(module, **kwargs)
 
 if __name__ == '__main__':
     ramp()
