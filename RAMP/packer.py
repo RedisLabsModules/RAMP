181a15c9618f35077cedf6c19c157639f3ecb3a3
Rafi Einstein <rafi@redislabs.com>
2020-04-21 09:37:41 +0300

    Added name field to dependencies (#51)

--- RAMP/packer.py
+++ RAMP/packer.py
@@ -126,12 +126,9 @@ def package(module, **args):
 
     # validate dependencies
     for dep in metadata["dependencies"]:
-        if len(dep) != 2:
-            raise Exception("error: a dependency should specify both URI and SHA256")
-        if "url" not in dep:
-            raise Exception("error: dependency missing url")
-        if "sha256" not in dep:
-            raise Exception("error: dependency missing sha256")
+        for key in ["name", "url", "sha256"]:
+            if key not in dep:
+                raise Exception("error: dependency missing {}".format(key))
 
     try:
         p = Popen('git rev-parse HEAD'.split(' '), stdin=PIPE, stdout=PIPE, stderr=PIPE)
