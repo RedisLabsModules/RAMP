eb066c844494be9305587d12d04a03708c6ccb2a
rafie <rafi@redislabs.com>
2020-05-11 09:37:49 +0300

    fixes 4

--- RAMP/ramp.py
+++ RAMP/ramp.py
@@ -97,7 +97,7 @@ def unpack(bundle):
 @click.option('--dependencies', callback=jsons_str_tuple_to_jsons_tuple, help='list of module dependencies: <name, uri, sha256>')
 @click.option('--output', '-o', default='module.zip', help='output file name')
 @click.option('--print-filename-only', '-P', is_flag=True, default=False, help="Print package path, but don't generate file")
-@click.option('--packname-file', default=None, help="Print package name to the file")
+@click.option('--packname-file', default=None, help="Write package name to the file")
 @click.option('--verbose', '-v', is_flag=True, default=False, help='verbose mode: print the resulting metadata')
 @click.option('--debug', is_flag=True, default=False, help='Print interaction with Redis. Implies --verbose.')
 def pack(module, *args, **kwargs):
