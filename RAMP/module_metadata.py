078ff15570a393920515799d698075a381168876
meir <meir@redis.com>
2023-01-19 12:09:55 +0200

    Added support for os_list field.

--- RAMP/module_metadata.py
+++ RAMP/module_metadata.py
@@ -3,6 +3,7 @@ import hashlib
 import platform
 from typing import List, Dict, Any  # noqa: F401
 from .common import *
+import distro
 
 
 # Defaults
@@ -62,7 +63,8 @@ FIELDS = [
     "sha256",
     "version",
     "crdb",
-    "redis_args"
+    "redis_args",
+    "os_list"
 ]  # type: List[str]
 
 
@@ -75,6 +77,16 @@ def sha256_checksum(filename, block_size=65536):
             sha256.update(block)
     return sha256.hexdigest()
 
+OS_MAP = {
+    "centos7": "rhel7",
+    "centos8": "rhel8",
+    "rocky8": "rhel8",
+}
+
+def get_curr_os():
+    global OS_MAP
+    curr_os = '%s%s' % (distro.id(), distro.version())
+    return OS_MAP.get(curr_os, curr_os)
 
 def create_default_metadata(module_path):
     # type: (str) -> Dict[str, Any]
@@ -108,4 +120,5 @@ def create_default_metadata(module_path):
         "add_command": ADD_COMMAND,
         "crdb": CRDB_ARGS,
         "redis_args": REDIS_ARGS,
+        "os_list": [get_curr_os()],
     }
